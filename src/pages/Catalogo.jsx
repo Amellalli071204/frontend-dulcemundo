816f756ab5503414bb25d8d6c07d3a0dcc22c649
Amellalli <amellallimiranda712@gmail.com>
2026-02-05 15:35:08 -0600

    Fix: Ruta de Catalogo.css corregida

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../Catalogo.css'; // ¡Aquí conectamos el estilo!
+import './Catalogo.css'; // ¡Aquí conectamos el estilo!
 
 export default function Catalogo() {
   const [productos, setProductos] = useState([]);
